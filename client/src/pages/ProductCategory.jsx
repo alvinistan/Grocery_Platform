5acc9d4517ca6f4e97f5a55fdd6fe232dff79b3e
Alvinistan <alvinistan@students.nsbm.ac.lk>
2026-02-14 11:20:55 +0530

    product Category page name

---
+++ client/src/pages/ProductCategory.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useAppContext } from '../context/AppContext';
+import { useParams } from 'react-router-dom';
+import { categories } from '../assets/assets';
+
+const ProductCategory = () => {
+
+    const {products} = useAppContext();
+    const { category} = useParams();
+
+    const searchCategory = categories.find((item) => item.path.toLocaleLowerCase() === category);
+    
+    const filteredProducts = products.filter((product) => product.category.toLocaleLowerCase() === category)
+  return (
+    <div className='mt-16'>
+        {
+            searchCategory && (
+                <div className='flex flex-col items-end w-max'>
+                    <p className='text-2xl font-medium'>{searchCategory.text}</p>
+                    <div className='w-16 h-0.5 bg-primary rounded-full'></div>
+                </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default ProductCategory
